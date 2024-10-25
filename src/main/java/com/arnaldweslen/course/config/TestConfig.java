c3be3df5c3f7c7506341247ba23c245bc1219539
ArnaldWelen05 <arnaldclash@gmail.com>
2024-10-25 20:15:59 -0300

    Payment, one-to-one association

--- src/main/java/com/arnaldweslen/course/config/TestConfig.java
+++ src/main/java/com/arnaldweslen/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.arnaldweslen.course.entites.Category;
 import com.arnaldweslen.course.entites.Order;
 import com.arnaldweslen.course.entites.OrderItem;
+import com.arnaldweslen.course.entites.Payment;
 import com.arnaldweslen.course.entites.Product;
 import com.arnaldweslen.course.entites.User;
 import com.arnaldweslen.course.entites.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		oderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
